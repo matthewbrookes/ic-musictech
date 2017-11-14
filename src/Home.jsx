a7e0947af8926ffca9ed10a1e482d7b462b0d02c
Matthew Brookes <mbrookes1304@gmail.com>
2017-11-14 21:18:04 +0000

    Create web structure

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => (
+    <p>Join Musictech Imperial if you're interested in learning how to DJ or produce music. DJs can play at a number of live events over the course of their university career and producers can get help learning the theory behind making great music. It doesn't matter what type of music you are into, Musictech Imperial will show you how to make it or how to play it live in a club.</p>
+);
+
+export default Home;
