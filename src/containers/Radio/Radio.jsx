16923e466236b5f3efeca4f37cc285efbcb36a42
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-09 20:29:29 +0000

    Download photos used on events/radio pages

--- src/containers/Radio/Radio.jsx
+++ src/containers/Radio/Radio.jsx
@@ -13,14 +13,14 @@ const shows = [
                    "We'll regularly have hand-picked DJs in the mix. We aim to keep the slots brief, giving a platform to showcase the very best of what they're mixing. " +
                    "Alongside this, our production showcase will focus on the newest tracks our members have been working on. " +
                    "The aim is to keep this fresh, reflecting the full spectrum of the UK electronic music scene.",
-    "image": "http://i.imgur.com/NfMSms2h.jpg",
+    "image": "/images/radio-shows/musictech-sessions.jpg",
     "date": "Various times throughout the year"
   },
   {
     "title": "The Big Daddy Hour",
     "description": "Hear the latest, up and coming tunes from the world of music beyond the charts and delve into the underground world of electronica.",
     "date": "Thursday 19:00-20:00",
-    "image": "https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/23213334_10155148649867362_5281441382595542657_o.jpg?oh=2da0e66fdb69d58d38e178f0628f8334&oe=5A96F3CF"
+    "image": ""
   }, {
     "title": "The Little Sus Hour",
     "description": "",
