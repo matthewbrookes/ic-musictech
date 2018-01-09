d66cf98e3498f3f60752fa72b0c2d8fa231bfcda
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-09 18:32:25 +0000

    Update Home Page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Img = styled.img`
+  height: 100%;
+  width:  100%;
+`;
+
+const MainImgWrapper = styled.div`
+  width: 400px;
+  display: block;
+  margin: 0 auto;
+`;
+
+const SecondaryImgWrapper = styled.div`
+  width: 600px;
+  display: block;
+  margin: 0 auto;
+`;
 
 const Home = () => (
+  <Wrapper>
     <p>Join Musictech Imperial if you're interested in learning how to DJ or produce music. DJs can play at a number of live events over the course of their university career and producers can get help learning the theory behind making great music. It doesn't matter what type of music you are into, Musictech Imperial will show you how to make it or how to play it live in a club.</p>
+    <MainImgWrapper>
+      <Img src="/images/good_form.jpg" />
+    </MainImgWrapper>
+    <SecondaryImgWrapper>
+      <Img src="/images/kieran-francesco-harry.jpg" />
+    </SecondaryImgWrapper>
+  </Wrapper>
 );
 
 export default Home;
