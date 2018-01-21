2d0c217b0421fb17141bbd6d672b5bc7046c9292
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-21 15:45:09 +0000

    Create form for signup

--- src/containers/Signup/Signup.jsx
+++ src/containers/Signup/Signup.jsx
@@ -24,12 +24,57 @@ if (process.env.NODE_ENV === "production") {
 
 const Wrapper = styled.div``;
 
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 250px;
+  margin: 0 auto;
+  padding-bottom: 10px;
+`;
+
+const LeftAlignWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-start;
+`;
+
+const Label = styled.label`
+  align: left;
+  font-size: 11px;
+  color: #3e3e3e;
+  margin-top: 10px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+`;
+
+const SlotWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
 class Signup extends React.Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      slot: "slot1",
+    };
+    this.slots = {
+      slot1: "7pm - 8pm",
+      slot2: "8pm - 9pm",
+      slot3: "9pm - 10pm"
+    };
+
+
     this.fetchSession = this.fetchSession.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleGenreChange = this.handleGenreChange.bind(this);
+    this.handlePartnerChange = this.handlePartnerChange.bind(this);
+    this.handleSlotChange = this.handleSlotChange.bind(this);
 
     this.fetchSession();
   }
@@ -40,43 +85,106 @@ class Signup extends React.Component {
     let component = this;
 
     var params = {
-        TableName : "musictech-sessions",
-        KeyConditionExpression: "#name = :session",
-        ExpressionAttributeNames:{
-          "#name": "name"
-        },
-        ExpressionAttributeValues: {
-          ":session":"session"
-        },
-        Limit: 1,
-        ScanIndexForward: false
+      TableName : "musictech-sessions",
+      KeyConditionExpression: "#name = :session",
+      ExpressionAttributeNames:{
+        "#name": "name"
+      },
+      ExpressionAttributeValues: {
+        ":session":"session"
+      },
+      Limit: 1,
+      ScanIndexForward: false
     };
 
     docClient.query(params, function(err, data) {
-        if (err) {
-            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-            component.setState({
-              location: 'Metric',
-              date: 'Next Monday'
-            })
-        } else {
-            data.Items.forEach(function(item) {
-              component.setState({
-                location: item['location'],
-                date: item['session-date'],
-              });
-            });
-        }
+      if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        component.setState({
+          location: "Metric",
+          date: "Next Monday"
+        });
+      } else {
+        data.Items.forEach(function(item) {
+          component.setState({
+            location: item["location"],
+            date: item["session-date"],
+          });
+        });
+      }
     });
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handleGenreChange(e) {
+    this.setState({
+      genre: e.target.value,
+    });
+  }
+
+  handlePartnerChange(e) {
+    this.setState({
+      partner: e.target.value,
+    });
+  }
+
+  handleSlotChange(e) {
+    this.setState({
+      slot: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
   }
 
   render() {
     return (
-    <Wrapper>
-      <p>Sign here</p>
-      <Separator />
-      <SessionInfo date={this.state.date} location={this.state.location} />
-    </Wrapper>
+      <Wrapper>
+        <p>Sign here</p>
+        <Separator />
+        <SessionInfo date={this.state.date} location={this.state.location} />
+        <FormWrapper>
+          <form onSubmit={this.handleSubmit}>
+            <LeftAlignWrapper>
+              <Label for="signupname">Name: </Label>
+            </LeftAlignWrapper>
+            <Input type="text" id="signupname" onChange={this.handleNameChange} required />
+            <LeftAlignWrapper>
+              <Label for="genre">Genre: </Label>
+            </LeftAlignWrapper>
+            <Input type="text" id="genre" onChange={this.handleGenreChange} required />
+            <LeftAlignWrapper>
+              <Label for="partner">Partner (optional): </Label>
+            </LeftAlignWrapper>
+            <Input type="text" id="partner" onChange={this.handlePartnerChange} />
+            <LeftAlignWrapper>
+              <Label>Choose a slot:</Label>
+            </LeftAlignWrapper>
+            <SlotWrapper>
+              <LeftAlignWrapper>
+                <input type="radio" id="slot1" value="slot1" onChange={this.handleSlotChange} checked={this.state.slot === "slot1"}/>
+                <label htmlFor="slot1">{this.slots["slot1"]}</label>
+              </LeftAlignWrapper>
+              <LeftAlignWrapper>
+                <input type="radio" id="slot2" value="slot2" onChange={this.handleSlotChange} checked={this.state.slot === "slot2"}/>
+                <label htmlFor="slot2">{this.slots["slot2"]}</label>
+              </LeftAlignWrapper>
+              <LeftAlignWrapper>
+                <input type="radio" id="slot3" value="slot3" onChange={this.handleSlotChange} checked={this.state.slot === "slot3"}/>
+                <label htmlFor="slot3">{this.slots["slot3"]}</label>
+              </LeftAlignWrapper>
+            </SlotWrapper>
+            <button>Signup</button>
+          </form>
+        </FormWrapper>
+      </Wrapper>
     );
   }
 }
